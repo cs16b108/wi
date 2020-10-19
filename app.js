d9fe7caef050d732489769995500907fd0b31fdf
cs16b108 <cs16b108@smail.iitm.ac.in>
2020-10-19 23:06:35 +0530

    2

--- app.js
+++ app.js
@@ -26,25 +26,39 @@ con.connect(function(err) {
    	con.query("SELECT * FROM login where name in " +"('kaar','r')", function (err, result, fields) {
 		if (err) throw err;
 		console.log((result))
-		if (result[0])console.log("exists");
+		if (result[0])console.log("exists",result.length, result,result[0]);
+		if (result[3])console.log("exists3",result.length, result,result[0]);
+
 	}); 
   });
 });
 
 app.get('/',(req,res) =>{
-	res.send("Hello World")
+	res.send('Hello World')
+
 })
 
-app.get('app/user',(req,res) =>{
-	req.body.username
-	con.query("SELECT * FROM login where name = " +req.body.name, function (err, result, fields) {
+app.get('/app/user',(req,res) =>{
+	Q = "SELECT * FROM login where name in ('" +req.headers.user+"');" ;
+	console.log(Q)
+	con.query(Q, function (err, result, fields) {
 		if (err) throw err;
-		if (len(res)==1)console.log("exists");
+		if (result.length==1){
+			console.log("exists");
+			res.send('Already Exists')
+		}
+		else {
+			Q = "INSERT INTO login values ("+
+  			con.query("INSERT INTO login values ", function (err, result, fields) {
+    			if (err) throw err;
+    			console.log(result);
+  			res.send('Done')
+
+  			});			
+		}
 	});
-  	con.query("INSERT INTO login ", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  	});
+
+
 })
 
 app.get('/app/user/auth',(req,res) =>{
